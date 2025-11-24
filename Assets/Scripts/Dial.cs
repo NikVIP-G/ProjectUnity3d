63e2da5f8aeee5e79728c8d9a635681707223af2
NikVIP-G <68080495+NikVIP-G@users.noreply.github.com>
2025-11-24 10:44:03 +0300

    Update and rename Rotation.cs to Dial.cs

--- Assets/Scripts/Dial.cs
+++ Assets/Scripts/Dial.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotation : MonoBehaviour
+public class Dial : MonoBehaviour
 {
     [SerializeReference] private float _speed;
     
-    void Update()
+    private void Update()
     {
         transform.Rotate(_speed * Vector3.up * Time.deltaTime);
     }
