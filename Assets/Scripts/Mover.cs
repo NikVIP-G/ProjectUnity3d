f31dd1e360160fe87c81e16c48707bc232844a76
NikVIP-G <68080495+NikVIP-G@users.noreply.github.com>
2025-11-24 10:42:11 +0300

    Update and rename MoveForward.cs to Mover.cs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveForward : MonoBehaviour
+public class Mover : MonoBehaviour
 {
     private void Update()
     {
